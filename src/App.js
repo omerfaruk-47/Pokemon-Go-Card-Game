c3fd0dba5a7646f41a9493b7dfefb3c3833a1563
omerfaruk-47 <ozcelikomerfaruk.4721@gmail.com>
2024-01-26 23:47:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Pokecard from "./Pokecard";
+import "./Pokecard.css";
+import "./App.css";
+import Pokegane from "./Pokegane";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Go Card Game</h1>
+      <Pokegane />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pokedex.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Pokecard from "./Pokecard";
+import "./Pokedex.css";
+
+class Pokedex extends Component {
+  render() {
+    let title;
+
+    if (this.props.isWinner) {
+      title = <h2 className="Pokodex-winner">Winning Player</h2>;
+    } else {
+      title = <h2 className="Pokodex-loser">Winning Player</h2>;
+    }
+
+    return (
+      <div className="Pokedex">
+        {title}
+        <h4>Total Exprience:{this.props.exp}</h4>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default Pokedex;
